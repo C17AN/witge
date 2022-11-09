7d84973ed2e02c94cc8cc31128d71c3d7df35f24
HD152513 <qag331@naver.com>
2022-11-10 02:08:28 +0900

    style: navbar & searchField

--- components/common/Navbar.jsx
+++ components/common/Navbar.jsx
@@ -2,15 +2,45 @@ import Link from "next/link";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import Image from 'next/image';
+import { css } from "@emotion/react";
+import SmallButton from "../common/SmallButton"
 
 function Navigation() {
   return (
-    <Navbar bg="dark" variant="dark">
-      <Container>
-        <Nav className="me-auto">
-          <Link href="/" passHref>
-            <Navbar.Brand href="#home">WitG</Navbar.Brand>
-          </Link>
+    <Navbar bg="dark" variant="dark"
+      css={css`
+          background-color: #030318 !important;
+          font-size: 18px !important;
+      `}
+    >
+      <Nav
+        css={css`
+          justify-content: space-between !important;
+          width: 100%;
+          padding: 0 15vw;
+          a, button {
+            margin: auto 0;
+          }
+        `}
+      >
+        <Link href="/" passHref>
+          <Navbar.Brand href="#home"
+            css={css`
+              margin: 0 !important;
+              flex: 1;
+            `}
+          >
+            <Image src="/logo.svg" alt="witg logo" width={130} height={45}/>
+          </Navbar.Brand>
+        </Link>
+        <div
+          css={css`
+            display: flex;
+            justify-content: space-between !important;
+            flex: 2;
+          `}
+        >
           <Link href="/parties" passHref>
             <Nav.Link>파티 참여</Nav.Link>
           </Link>
@@ -26,9 +56,9 @@ function Navigation() {
           <Link href="/apply" passHref>
             <Nav.Link>리더 지원</Nav.Link>
           </Link>
-          <button>LOGIN</button>
-        </Nav>
-      </Container>
+          <SmallButton icon="BsLock" value="LOGIN" theme="none"/>
+        </div>
+      </Nav>
     </Navbar>
   );
 }
--- pages/leaders.jsx
+++ pages/leaders.jsx
@@ -14,9 +14,9 @@ const leaders = () => {
       <TextButton />
       <ControlledTabs />
       <StarRating rating={4.5} />
-      <SmallButton icon="BsFillChatFill" value="문의하기" />
-      <SmallButton icon="BsBookmarkStarFill" value="스크랩하기" />
-      <SmallButton icon="BsShareFill" value="공유하기" />
+      <SmallButton icon="BsFillChatFill" value="문의하기" theme="gradation" />
+      <SmallButton icon="BsBookmarkStarFill" value="스크랩하기" theme="light" />
+      <SmallButton icon="BsShareFill" value="공유하기" theme="light" />
     </div>
   );
 };
