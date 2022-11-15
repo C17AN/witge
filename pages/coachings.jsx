975c4043f41957485cbae2e9dcc7442efb617d7e
chanmin <chanmin@toss.im>
2022-11-15 21:56:00 +0900

    feat: 목표 설정 마크업 추가

--- pages/coachings.jsx
+++ pages/coachings.jsx
@@ -52,7 +52,146 @@ const coachings = () => {
         pageDescription="코칭을 원하는 수강생의 목표와 스타일을 입력하여 자신에게 어울리는 리더를 추천받을 수 있습니다."
       />
       <Container>
-        <Content></Content>
+        <Content>
+          <h2
+            css={css`
+              font-size: 28px;
+              margin-bottom: 42px;
+            `}
+          >
+            <span
+              css={css`
+                color: rgb(72, 72, 212);
+              `}
+            >
+              STEP 1.
+            </span>{" "}
+            목표를 설정해 봅시다.
+          </h2>
+          <div
+            css={css`
+              display: flex;
+              justify-content: space-evenly;
+              margin-bottom: 64px;
+            `}
+          >
+            <div>
+              <div
+                css={css`
+                  font-weight: 600;
+                  font-size: 20px;
+                `}
+              >
+                나의 현재 등급은?
+              </div>
+              <InputWrapper>
+                <RadioButton type="radio" />
+                <div>Iron</div>
+              </InputWrapper>
+              <InputWrapper>
+                <RadioButton type="radio" />
+                <div>Bronze</div>
+              </InputWrapper>
+              <InputWrapper>
+                <RadioButton type="radio" />
+                <div>Silver</div>
+              </InputWrapper>
+              <InputWrapper>
+                <RadioButton type="radio" />
+                <div>Gold</div>
+              </InputWrapper>
+              <InputWrapper>
+                <RadioButton type="radio" />
+                <div>Platinum</div>
+              </InputWrapper>
+              <InputWrapper>
+                <RadioButton type="radio" />
+                <div>Diamond</div>
+              </InputWrapper>
+              <InputWrapper>
+                <RadioButton type="radio" />
+                <div>모름</div>
+              </InputWrapper>
+            </div>
+            <div>
+              <div
+                css={css`
+                  font-weight: 600;
+                  font-size: 20px;
+                `}
+              >
+                나의 현재 등급은?
+              </div>
+              <InputWrapper>
+                <RadioButton type="radio" />
+                <div>Iron</div>
+              </InputWrapper>
+              <InputWrapper>
+                <RadioButton type="radio" />
+                <div>Bronze</div>
+              </InputWrapper>
+              <InputWrapper>
+                <RadioButton type="radio" />
+                <div>Silver</div>
+              </InputWrapper>
+              <InputWrapper>
+                <RadioButton type="radio" />
+                <div>Gold</div>
+              </InputWrapper>
+              <InputWrapper>
+                <RadioButton type="radio" />
+                <div>Platinum</div>
+              </InputWrapper>
+              <InputWrapper>
+                <RadioButton type="radio" />
+                <div>Diamond</div>
+              </InputWrapper>
+              <InputWrapper>
+                <RadioButton type="radio" />
+                <div>모름</div>
+              </InputWrapper>
+            </div>
+            <div>
+              <div
+                css={css`
+                  font-weight: 600;
+                  font-size: 20px;
+                `}
+              >
+                나의 현재 등급은?
+              </div>
+              <InputWrapper>
+                <RadioButton type="radio" />
+                <div>Iron</div>
+              </InputWrapper>
+              <InputWrapper>
+                <RadioButton type="radio" />
+                <div>Bronze</div>
+              </InputWrapper>
+              <InputWrapper>
+                <RadioButton type="radio" />
+                <div>Silver</div>
+              </InputWrapper>
+              <InputWrapper>
+                <RadioButton type="radio" />
+                <div>Gold</div>
+              </InputWrapper>
+              <InputWrapper>
+                <RadioButton type="radio" />
+                <div>Platinum</div>
+              </InputWrapper>
+              <InputWrapper>
+                <RadioButton type="radio" />
+                <div>Diamond</div>
+              </InputWrapper>
+              <InputWrapper>
+                <RadioButton type="radio" />
+                <div>모름</div>
+              </InputWrapper>
+            </div>
+          </div>
+          <NextStepButton>{"다음 단계로 >>"}</NextStepButton>
+        </Content>
       </Container>
     </div>
   );
@@ -80,7 +219,7 @@ const Container = styled.div`
 `;
 
 const Content = styled.div`
-  padding: 32px;
+  padding: 40px;
   box-shadow: 4px 4px 4px 4px rgba(0, 0, 0, 0.15);
   border-top-left-radius: 20px;
   border-bottom-right-radius: 20px;
@@ -89,11 +228,38 @@ const Content = styled.div`
   display: flex;
   flex-direction: column;
   width: 960px;
-  height: 800px;
 
   &:last-of-type {
     margin-bottom: 48px;
   }
+`;
+
+const InputWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const RadioButton = styled.input`
+  margin: 12px 8px 12px 0;
+`;
+
+const NextStepButton = styled.button`
+  border-radius: 24px;
+  padding: 12px 0;
+  outline: none;
+  border: none;
+  color: white;
+  background: rgb(76, 72, 227);
+  width: 60%;
+  margin: 0 auto;
+  min-width: 480px;
+  font-weight: 700;
+  background: linear-gradient(
+    90deg,
+    rgba(76, 72, 227, 1) 0%,
+    rgba(27, 224, 81, 1) 100%,
+    rgba(0, 212, 255, 1) 100%
+  );
 `;
 
 export default coachings;
